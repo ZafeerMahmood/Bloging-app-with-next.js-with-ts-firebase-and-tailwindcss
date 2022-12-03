ba7b37692482d7cf7eaa8395e3ecba535c0de067
ZafeerMahmoodm <zafeer7460@gmail.com>
2022-12-03 14:21:18 +0500

    removed public api keys

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_APIkEY,
-  authDomain: "blog-app-5a483.firebaseapp.com",
-  projectId: "blog-app-5a483",
-  storageBucket: "blog-app-5a483.appspot.com",
-  messagingSenderId: "353236105742",
-  appId: "1:353236105742:web:6d58bba9ac0f166ee19a5f",
-  measurementId: "G-EXKFMXVJVK"
+  apiKey: process.env.NEXT_PUBLIC_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_authDomain,
+  projectId: process.env.NEXT_PUBLIC_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_appId,
+  measurementId: process.env.NEXT_PUBLIC_measurementId,
 };
    
 
